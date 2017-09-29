3d6e21837273d51e4a7fae63a6b16991ccfbb743
HanFengqi <hanfengqi@yqj.cn>
2017-09-29 16:18:05 +0800

    实现下载

--- app/src/main/java/win/tommy/tusha/ui/activity/BigPicActivity.java
+++ app/src/main/java/win/tommy/tusha/ui/activity/BigPicActivity.java
@@ -1,18 +1,53 @@
 package win.tommy.tusha.ui.activity;
 
+import android.Manifest;
+import android.os.Environment;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 import uk.co.senab.photoview.PhotoView;
 import win.tommy.tusha.R;
 import win.tommy.tusha.base.BaseActivity;
 import win.tommy.tusha.model.bean.PictureBean;
+import win.tommy.tusha.util.ToastUtil;
 import win.tommy.tusha.util.glide.GlideImageLoader;
+import win.tommy.tusha.widget.progress.HorizontalProgressBarWithNumber;
+import zlc.season.rxdownload2.RxDownload;
+import zlc.season.rxdownload2.entity.DownloadEvent;
+import zlc.season.rxdownload2.entity.DownloadFlag;
 
 public class BigPicActivity extends BaseActivity {
 
 
     private PhotoView photo_img;
+    private ImageView photo_download;
+    private int position;
+    private List<PictureBean.ResultsBean> results;
+    private HorizontalProgressBarWithNumber photo_progress;
+    private static final int MSG_PROGRESS_UPDATE = 0x110;
+    private Handler mHandler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            int progress = photo_progress.getProgress();
+//            int roundProgress = mRoundProgressBar.getProgress();
+            photo_progress.setProgress(++progress);
+//            mRoundProgressBar.setProgress(++roundProgress);
+            if (progress >= 100) {
+                mHandler.removeMessages(MSG_PROGRESS_UPDATE);
+            }
+            mHandler.sendEmptyMessageDelayed(MSG_PROGRESS_UPDATE, 100);
+        }
+    };
+    private RxDownload rxDownload;
 
     @Override
     public int getLayoutId() {
@@ -27,12 +62,76 @@ public class BigPicActivity extends BaseActivity {
     @Override
     public void initView() {
         photo_img = (PhotoView) findViewById(R.id.photo_img);
+        photo_download = (ImageView) findViewById(R.id.photo_download);
+        photo_progress = (HorizontalProgressBarWithNumber) findViewById(R.id.photo_progress);
     }
 
     @Override
     protected void initData() {
-        int position = getIntent().getIntExtra("position", 0);
-        List<PictureBean.ResultsBean> results = (ArrayList<PictureBean.ResultsBean>) getIntent().getSerializableExtra("results");
-        GlideImageLoader.getInstance().displayImage(this,results.get(position).getUrl(),photo_img);
+        position = getIntent().getIntExtra("position", 0);
+        results = (ArrayList<PictureBean.ResultsBean>) getIntent().getSerializableExtra("results");
+        GlideImageLoader.getInstance().displayImage(this, results.get(position).getUrl(), photo_img);
+
+        rxDownload = RxDownload.getInstance(this)
+                //.retrofit(myRetrofit)             //若需要自己的retrofit客户端,可在这里指定
+                .defaultSavePath(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath()) //设置默认的下载路径
+                .maxThread(3)                     //设置最大线程
+                .maxRetryCount(3)                 //设置下载失败重试次数
+                .maxDownloadNumber(5);
+
+
+        photo_download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //开始下载
+                new RxPermissions(BigPicActivity.this)
+                        .request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        .doOnNext(new Consumer<Boolean>() {
+                            @Override
+                            public void accept(Boolean aBoolean) throws Exception {
+                                if (!aBoolean) {  //权限被拒绝
+                                    ToastUtil.showLongToast(BigPicActivity.this, "权限被拒绝!");
+                                }
+                            }
+                        })
+                        .subscribeOn(Schedulers.io())
+                        .compose(rxDownload.<Boolean>transformService(results.get(position).getUrl()))  //download
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(new Consumer<Object>() {
+                                       @Override
+                                       public void accept(Object o) throws Exception {
+//                                           ToastUtil.showLongToast(BigPicActivity.this,"开始下载");
+                                           photo_progress.setVisibility(View.VISIBLE);
+                                       }
+                                   }, new Consumer<Throwable>() {
+                                       @Override
+                                       public void accept(Throwable throwable) throws Exception {
+                                           ToastUtil.showLongToast(BigPicActivity.this, "添加任务失败");
+                                       }
+                                   }
+                        );
+                //接受接收下载事件和下载状态
+                rxDownload.receiveDownloadStatus(results.get(position).getUrl())
+                        .subscribe(new Consumer<DownloadEvent>() {
+                            @Override
+                            public void accept(DownloadEvent event) throws Exception {
+                                //当事件为Failed时, 才会有异常信息, 其余时候为null.
+                                if (event.getFlag() == DownloadFlag.FAILED) {
+                                    Throwable throwable = event.getError();
+                                    Log.e("twb", "Errore==" + throwable);
+                                    ToastUtil.showLongToast(BigPicActivity.this, "下载出错");
+                                }
+                                long percentNumber = event.getDownloadStatus().getPercentNumber();
+                                photo_progress.setProgress((int) percentNumber);
+                                if (percentNumber == 100) {
+                                    photo_progress.setVisibility(View.GONE);
+                                    ToastUtil.showLongToast(BigPicActivity.this, "下载完成");
+                                }
+                            }
+                        });
+            }
+        });
+
+
     }
 }
